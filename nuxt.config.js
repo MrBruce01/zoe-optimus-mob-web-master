b5c31941cf26355260494bfb8188dc1976eff125
MrBruce01 <44419994+MrBruce01@users.noreply.github.com>
2019-05-06 18:03:20 +0800

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the pageaxios
   */
   head: {
-    title: '智业移动护理',
+    title: '练习项目',
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
