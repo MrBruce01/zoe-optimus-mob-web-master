cd49dce45f8bcb2f1ab7f9ad6c679e685e9baf11
MrBruce01 <44419994+MrBruce01@users.noreply.github.com>
2019-05-06 17:53:38 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,9 +1,6 @@
 const config = {
   default: {
-    //baseURL: 'http://192.168.2.191:8080/zoe-optimus-mobile-web/',
-    // baseURL: 'http://117.25.173.18:7008/zoe-optimus-mobile-web/',
     baseURL: 'http://192.168.2.164:7008/zoe-optimus-mobile-web/',
-    // baseURL: 'http://10.0.1.174:8081/zoe-optimus-mobile-web/',
     timeout: 60000,
     headers: {
       'deptCode': '1010320',
@@ -15,7 +12,6 @@ const config = {
     }
   },
   his5: {
-    // baseURL: 'http://192.168.2.226:8082/zoe-optimus-web-api/'
       baseURL: 'http://192.168.2.191:8080/zoe-optimus-mobile-web/'
   }
 }
